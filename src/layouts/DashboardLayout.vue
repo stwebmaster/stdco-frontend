10120da7975dfe42d1ec8f12ef2a664d55c6ab42
Ahmad Rezaei <parsarezaei53@gmail.com>
2023-01-03 19:54:29 +0330

    login bug fix

--- src/layouts/DashboardLayout.vue
+++ src/layouts/DashboardLayout.vue
@@ -1,12 +1,19 @@
 <script setup lang="ts">
 import Sidebar from "@/components/Sidebar.vue";
+import { useAuthStore } from "@/stores/auth";
+import Loading from "@/components/Loading.vue";
+
+const auth = useAuthStore();
 </script>
 
 <template>
-  <div>
+  <div v-if="auth.isLoggedIn">
     <Sidebar />
     <div class="relative min-h-screen bg-slate-100 md:ml-64">
       <slot></slot>
     </div>
+  </div>
+  <div v-else class="grid min-h-screen items-center justify-center">
+    <Loading />
   </div>
 </template>
